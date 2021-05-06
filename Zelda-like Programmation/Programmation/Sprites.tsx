42626ee2f1256fc458a2da4ec6a6f89ea63f7235
ThibaultJOVELIN <74654940+ThibaultJOVELIN@users.noreply.github.com>
2021-05-06 17:24:47 +0200

    Finalisation des maps, Changement du code (le jeu de ne marche plus)

--- Zelda-like Programmation/Programmation/Sprites.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="SpritesSheetComplet" tilewidth="32" tileheight="32" tilecount="112" columns="7">
- <image source="../Projet Zelda-like/Dossiers de travail/Sprites/SpritesSheetComplet.png" width="224" height="512"/>
-</tileset>
